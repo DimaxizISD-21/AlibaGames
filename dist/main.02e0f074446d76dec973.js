94e439d8e541ea7636297994e3dae6a68a41d050
Oleksiy Nikolaenko <alosha.nikolanko333@gmail.com>
2022-02-08 11:03:44 +0200

    webpack starter

---
+++ dist/main.02e0f074446d76dec973.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var o=[].concat(["a","b","c"],["d","e"]);console.log(70),console.log(o)})();
+//# sourceMappingURL=main.02e0f074446d76dec973.js.map
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./sass/styles.scss";
+
+const sum = (a, b) => a + b;
+
+const arr = ["a", "b", "c"];
+
+const newArr = [...arr, "d", "e"];
+
+console.log(sum(20, 50));
+console.log(newArr);
